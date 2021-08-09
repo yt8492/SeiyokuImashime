686d844141c4b8df12c5e6ce15b73e56bf55519d
yt8492 <tmymyut2000@gmail.com>
2021-08-10 04:36:40 +0900

    analyze all history

--- src/background.ts
+++ src/background.ts
@@ -11,7 +11,7 @@ type PurchaseResult = {
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log(message)
     if (message == 'analyze') {
-        read2020Works(1).then(works => {
+        readAllWorks(1).then(works => {
             const result = {
                 worksCount: works.length,
                 totalPrice: works.map(w => w.price).reduce((previousValue, currentValue) => {
@@ -30,7 +30,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     return true
 })
 
-async function read2020Works(page: number): Promise<Array<Work>> {
+async function readAllWorks(page: number): Promise<Array<Work>> {
     const url = `https://www.dlsite.com/maniax/mypage/userbuy/=/type/all/start/all/sort/1/order/1/page/${page}`
     const result = await (await fetch(url, { mode: 'no-cors' })).text()
     const parser = new DOMParser()
@@ -44,15 +44,12 @@ async function read2020Works(page: number): Promise<Array<Work>> {
             price: parseInt(e.getElementsByClassName('work_price')[0].textContent!!.replace(/[^0-9]/, ''))
         }
     })
-    const last = works[works.length - 1]
     const ul = document.getElementsByClassName('page_no')[0]
         .getElementsByTagName('ul')
-    const hasNextPage = ul.length != 0 && Array.from(ul[0].children).find(e => e.textContent == '次へ') != null
-    const hasNext =  last.date.startsWith('2020') && hasNextPage
-    const filteredWorks = works.filter(w => w.date.startsWith('2020'))
-    if (hasNext) {
-        return filteredWorks.concat(await read2020Works(page + 1))
+    const hasNextPage = ul.length !== 0 && Array.from(ul[0].children).find(e => e.textContent === '次へ') !== undefined
+    if (hasNextPage) {
+        return works.concat(await readAllWorks(page + 1))
     } else {
-        return filteredWorks
+        return works
     }
 }
--- src/popup.ts
+++ src/popup.ts
@@ -9,13 +9,11 @@ analyzeButton.addEventListener('click', function () {
         if (result != null) {
             const purchaseResult = result as PurchaseResult
             this.style.display = 'none'
-            numbers.textContent = `2020年の購入作品数: ${purchaseResult.worksCount}`
+            numbers.textContent = `総購入作品数: ${purchaseResult.worksCount}`
             price.textContent = `総額: ${purchaseResult.totalPrice}円`
-            const tweetContent = `私のDLsite散財2020\n購入作品数: ${purchaseResult.worksCount}作品\n合計金額: ${purchaseResult.totalPrice}円\n`
+            const tweetContent = `私はDLsiteにこれだけ溶かしました\n購入作品数: ${purchaseResult.worksCount}作品\n合計金額: ${purchaseResult.totalPrice}円\n`
             tweetButton.style.display = 'block'
-            const gitHubUrl = 'https://github.com/yt8492/DLsiteSanzai2020'
-
-            tweetButton.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetContent)}&hashtags=${encodeURIComponent('DLsite散財2020')}&url=${encodeURIComponent(gitHubUrl)}`
+            tweetButton.href = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetContent)}`
         } else {
             this.textContent = '失敗しました。ページをリロードして再試行してください。'
         }
